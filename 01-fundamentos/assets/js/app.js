cabeed25b139c3490bd4d215afca9823c98155f4
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-08-15 18:13:51 -0500

    feat: javascript introduction and console

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 10;
+
+let c = 10, d = 10, e = 20;
+
+let f = 10,
+    g = 'hola ',
+    h = 'mundo',
+    i = 40;
+
+
+let x = a + b;
+
+const saludo = g + h;
+
+console.log( x );
+console.warn( x );
+console.error( x );
+
+console.log('%c Mis variables', 'color:#438; font-weight: bold')
+console.log('a:', a );
+console.log({ a });
+
+console.table({ f, g, h, i});
+console.log( saludo );
+
+g = 'Hola de nuevo ';
+
+
+var outerWidth = 1000;
+
